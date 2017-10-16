fb141eb296826e7ebb003cd1098a97548095dc50
doc2git <doc2git@yahoo.com>
2017-10-16 18:50:22 +0800

    以下下行实现在没有all远程别名的情况下将config.sh中的fetchServerPrefix设为默认的fetch-url;

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ gitServerAllPrefixes[1]="git@github.com:$user";
 gitServerAllPrefixes[2]="git@gitee.com:$user";
 
 # 配置fetchServerPrefix可以实现在没有all远程别名的情况下将config.sh中的fetchServerPrefix设为默认的fetch-url;
-# 如果现已有all远程别名,还是改.git/config比较方便，毕竟这种情况不会经常发生。tchSeverPrefix="git@github.com:$user";
+# 如果现已有all远程别名,还是改.git/config比较方便(将要设置的默认url放在最前面)，毕竟这种情况不会经常发生。tchSeverPrefix="git@github.com:$user";
 
 fetchSeverPrefix=${gitServerAllPrefixes[1]};
 
