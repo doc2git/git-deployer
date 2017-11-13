a6d38ec008bb6c549c93572c4649c83d0d970d64
ivuex <ivuex@yahoo.com>
2017-11-13 20:09:18 +0800

    1510574958677650501

--- config.sh
+++ config.sh
@@ -11,8 +11,8 @@ localRepoServerPathParent="/home/myrepository";
 declare -a gitServerAllPrefixes;
 gitServerAllPrefixes[0]="git@localhost:/home/myrepository";
 user=$(whoami);
-gitServerAllPrefixes[1]="git@github.com:$user";
-gitServerAllPrefixes[2]="git@gitee.com:$user";
+gitServerAllPrefixes[1]="git@github.com:ivuex";
+gitServerAllPrefixes[2]="git@gitee.com:ivuex";
 
 # 配置fetchServerPrefix可以实现在没有all远程别名的情况下将config.sh中的fetchServerPrefix设为默认的fetch-url;
 # 如果现已有all远程别名,还是改.git/config比较方便(将要设置的默认url放在最前面)，毕竟这种情况不会经常发生。tchSeverPrefix="git@github.com:$user";
